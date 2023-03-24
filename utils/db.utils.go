6abb0fee11a24376491ff7bb31b3a53af435dadf
LethalisUmbra <anibal.cabrera2000@gmail.com>
2023-03-24 18:10:08 -0300

    gcp

--- utils/db.utils.go
+++ utils/db.utils.go
@@ -8,6 +8,6 @@ var DB *sql.DB
 
 func OpenDB() error {
 	var err error
-	DB, err = sql.Open("mysql", "root:@tcp(localhost:3306)/sns-test?parseTime=true")
+	DB, err = sql.Open("mysql", "root:fVQdk;_p}$.y1(@,@tcp(test-sns-api:southamerica-west1:test-sns)/sns-test?parseTime=true")
 	return err
 }
